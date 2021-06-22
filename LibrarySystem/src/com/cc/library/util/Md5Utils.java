9b1c9c035b8149681ea9c49372bbb91b53903a68
71F54A01 <1911900673@qq.com>
2021-06-22 15:36:47 +0800

    Base64代替BASE64Encoder

--- LibrarySystem/src/com/cc/library/util/Md5Utils.java
+++ LibrarySystem/src/com/cc/library/util/Md5Utils.java
@@ -3,8 +3,7 @@ package com.cc.library.util;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import sun.misc.BASE64Encoder;
-
+import java.util.Base64;
 public class Md5Utils {
 
 	/**
@@ -16,9 +15,7 @@ public class Md5Utils {
 		try{
 			MessageDigest md = MessageDigest.getInstance("md5");
 			byte md5[] = md.digest(message.getBytes());
-			
-			BASE64Encoder encoder = new BASE64Encoder();
-			return encoder.encode(md5);
+			return  Base64.getEncoder().encodeToString(md5);
 		}catch(NoSuchAlgorithmException e){
 			throw new RuntimeException(e);
 		}
