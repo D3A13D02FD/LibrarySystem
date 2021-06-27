9956b3ac2f56d06b8a8dce141584d8f35b52e196
71F54A01 <1911900673@qq.com>
2021-06-27 23:10:01 +0800

    批量添加图书的excel模板缺少一行描述

---
+++ MyApplication2/app/src/main/java/cn/edu/ccit/myapplication/MainActivity.java
@@ -0,0 +1,96 @@
+package cn.edu.ccit.myapplication;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.view.Menu;
+
+/* import相关class */
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    /* 变量声明 */
+    private int intLevel;
+    private int intScale;
+    private Button mButton01;
+    private AlertDialog d;
+
+    /* create BroadcastReceiver */
+    private BroadcastReceiver mBatInfoReceiver=new BroadcastReceiver()
+    {
+        public void onReceive(Context context, Intent intent)
+        {
+            String action = intent.getAction();
+            /* 如果捕捉到的action是ACTION_BATTERY_CHANGED，
+             * 就执行onBatteryInfoReceiver() */
+            if (Intent.ACTION_BATTERY_CHANGED.equals(action))
+            {
+                intLevel = intent.getIntExtra("level", 0);
+                intScale = intent.getIntExtra("scale", 100);
+                onBatteryInfoReceiver(intLevel,intScale);
+            }
+        }
+    };
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* 初始化Button，并设定按下后的操作 */
+        mButton01 = (Button)findViewById(R.id.myButton1);
+        mButton01.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /* 注册几个系统 BroadcastReceiver，作为访问电池计量之用 */
+                registerReceiver
+                        (
+                                mBatInfoReceiver,
+                                new IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+                        );
+            }
+        });
+
+    }
+
+    /* 拦截到ACTION_BATTERY_CHANGED时要执行的method */
+    public void onBatteryInfoReceiver(int intLevel, int intScale)
+    {
+        /* create 弹出的对话窗口 */
+        d = new AlertDialog.Builder(MainActivity.this).create();
+        d.setTitle(R.string.str_dialog_title);
+        /* 将取得的电吕计量显示于Dialog中 */
+        d.setMessage(getResources().getString(R.string.str_dialog_body)+
+                String.valueOf(intLevel * 100 / intScale) + "%");
+        /* 设定返并加画面的按钮 */
+        d.setButton(getResources().getString(R.string.str_button2),
+                new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        /* 反注册Receiver，并关闭对话窗口 */
+                        unregisterReceiver(mBatInfoReceiver);
+                        d.dismiss();
+                    }
+                });
+        d.show();
+    }
+
+
+}
+
